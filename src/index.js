aab841a74ac8dc5e087a211c91e7df55450fa14d
Helder <helderlucau@gmail.com>
2023-06-23 16:06:32 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+  })
 });
+
+function listToDo(){
+
+}
+
+function listDelete(e){
+  e.target.parentNode.remove()
+}
