ce967b89adfa449e03a561aebab47365eae8c03c
Helder <helderlucau@gmail.com>
2023-06-23 21:23:46 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   let form = document.querySelector('#create-task-form')
-  form.addEventListener('submit', (e) => {
+    form.addEventListener('submit', (e) => {
     e.preventDefault()
-  })
+    toDoList(document.getElementById('new-task-description').value)
+    form.reset()
+   })
 });
-
-function listToDo(){
-
+// function to add tasks to the list 
+ function toDoList(task){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', listDelete)
+  btn.textContent = 'x'
+  li.textContent = `${task}`
+  li.appendChild(btn)
+  console.log(li)
+  document.querySelector('#tasks').appendChild(li)
 }
-
+// function to delete tasks from the list 
 function listDelete(e){
   e.target.parentNode.remove()
 }
